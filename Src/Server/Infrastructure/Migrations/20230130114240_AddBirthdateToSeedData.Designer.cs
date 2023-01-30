e11c109a904a3eca272272a525e8b2c34384d523
MeysamBahadori <bahadori.meysam@outlook.com>
2023-01-30 15:15:35 +0330

    Add date of birth to the seed data

--- Src/Server/Infrastructure/Migrations/20230130114240_AddBirthdateToSeedData.Designer.cs
+++ Src/Server/Infrastructure/Migrations/20230130114240_AddBirthdateToSeedData.Designer.cs
@@ -4,6 +4,7 @@ using Mc2.CrudTest.Infrastructure.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mc2.CrudTest.Infrastructure.Migrations
 {
     [DbContext(typeof(CrudTestReadWriteContext))]
-    partial class CrudTestReadWriteContextModelSnapshot : ModelSnapshot
+    [Migration("20230130114240_AddBirthdateToSeedData")]
+    partial class AddBirthdateToSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Src/Server/Infrastructure/Migrations/20230130114240_AddBirthdateToSeedData.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mc2.CrudTest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBirthdateToSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_Firstname_Lastname_DateOfBirth",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateOfBirth",
+                table: "Customers",
+                type: "datetimeoffset",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("09532446-967b-45e4-be15-013a4f03437e"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(2000, 6, 19, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("85259efb-0ca9-41d8-8159-e85c8535184f"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1988, 6, 19, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("8bd6dfea-6784-4b02-97c7-6d69b47e332a"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1991, 6, 19, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("e40156ae-992e-4fc1-a071-0bfacf8f427d"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1986, 6, 19, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Firstname_Lastname_DateOfBirth",
+                table: "Customers",
+                columns: new[] { "Firstname", "Lastname", "DateOfBirth" },
+                unique: true,
+                filter: "[Firstname] IS NOT NULL AND [Lastname] IS NOT NULL AND [DateOfBirth] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_Firstname_Lastname_DateOfBirth",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateOfBirth",
+                table: "Customers",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("09532446-967b-45e4-be15-013a4f03437e"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("85259efb-0ca9-41d8-8159-e85c8535184f"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("8bd6dfea-6784-4b02-97c7-6d69b47e332a"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("e40156ae-992e-4fc1-a071-0bfacf8f427d"),
+                column: "DateOfBirth",
+                value: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Firstname_Lastname_DateOfBirth",
+                table: "Customers",
+                columns: new[] { "Firstname", "Lastname", "DateOfBirth" },
+                unique: true,
+                filter: "[Firstname] IS NOT NULL AND [Lastname] IS NOT NULL");
+        }
+    }
+}
